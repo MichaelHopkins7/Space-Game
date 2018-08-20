b3825b91d37895400c50ba48e7c15cedf927c9c8
Michael Hopkins <LuckyMagnet77@gmail.com>
2018-08-20 11:25:29 -0400

    Initial Commit, creating variabls for use.

--- Space Game/Program.cs
+++ Space Game/Program.cs
@@ -10,6 +10,36 @@ namespace Space_Game
     {
         static void Main(string[] args)
         {
+
+            double startShipMCargoSpace = 6; // Setting ship max cargo capacities
+            double shipAMCargo = 8;
+            double shipBMCargo = 10;
+            double shipCMCargo = 12;
+
+            double shipCargoCurrent = 0; // variable for Cargo in ship now
+            double currentShipMCargo = startShipMCargoSpace; //sets cargo capacity to start
+
+            int creditsNow = 100; //creates value for storing currency amount and sets initial currency. Whole credits only.
+            int purchasePrice = 0; //sets up value to use for purchases
+
+            int startShipMSpeed = 4; // Setting ship max speeds
+            int shipAMSpeed = 6;
+            int shipBMSpeed = 7;
+            int shipCMSpeed = 9;
+            double 
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
