32910868241c0fdb90f36eebbfc9792981e1d409
Michael Hopkins <LuckyMagnet77@gmail.com>
2018-08-28 14:26:16 -0400

    Travel framework

--- Space Game/Travel.cs
+++ Space Game/Travel.cs
@@ -8,5 +8,23 @@ namespace Space_Game
 {
     class Travel
     {
+        public double distanceTraveled;
+        public int years;
+        public int weeks;
+        public int days;
+        public int hours;
+
+        public Travel(double distance, int years, int weeks, int days, int hours)
+        {
+            this.distanceTraveled = distance;
+            this.years = years;
+            this.weeks = weeks;
+            this.days = days;
+            this.hours = hours;
+        }
+
+
+
+
     }
 }
