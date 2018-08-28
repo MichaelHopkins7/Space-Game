b9a213eae108fd01ce4d6219f3e11b9ef3ca7418
Michael Hopkins <LuckyMagnet77@gmail.com>
2018-08-28 13:49:39 -0400

    building framework. does nothing

--- Space Game/Ship.cs
+++ Space Game/Ship.cs
@@ -8,20 +8,66 @@ namespace Space_Game
 {
     class Ship
     {
-        public int speed;
-        public int cargoSlots;
-        public int slotDepth;
+        private int speed = 3;
+        private int slots  = 6;
+        private int slotCapacity = 6;
+        private bool speedUp = true;
+        private bool capacityUp = true;
 
         public Ship()
         {
-            speed = 3;
-            cargoSlots = 12;
-            slotDepth = 6;
+            this.speed = 3;
+            this.slots = 12;
+            this.slotCapacity = 6;
         }
 
-        public void ShipUpgrade()
+        public int Speed()
         {
+            return speed;
+        }
 
+        public int cargoSlots()
+        {
+            return slots;
+        }
+
+        public int slotSize()
+        {
+            return slotCapacity;
+        }
+
+        public void ShipUpgrade(int choice)
+        {
+            switch (choice)
+            {
+                case 1:
+                    speed += 2;
+                    speedUp = true;
+                    break;
+                case 2:
+                    slotCapacity += 2;
+                    capacityUp = true;
+                    break;
+                case 3:
+                    speed = 4;
+                    slots = 16;
+                    speedUp = false;
+                    capacityUp = false;
+                    break;
+                case 4:
+                    speed = 6;
+                    slots = 20;
+                    slotCapacity = 10;
+                    speedUp = false;
+                    capacityUp = false;
+                    break;
+                case 5:
+                    speed = 8;
+                    slots = 24;
+                    speedUp = false;
+                    capacityUp = false;
+                    break;
+            }
         }
     }
 }
