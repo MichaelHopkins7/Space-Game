a118a167a5c52f1106c6050b9bcd82fab9629c59
Michael Hopkins <LuckyMagnet77@gmail.com>
2018-08-29 13:02:24 -0400

    completed Planet Name request

--- Space Game/Travel.cs
+++ Space Game/Travel.cs
@@ -17,7 +17,7 @@ namespace Space_Game
         // [Planet#, (0=X, 1=Y, 2=Name Part 1, 3=Name Part 2, 4=Name Part 3, 5=0 for pricing 6=Gold Price, 7=Iron Price, 8=Selenium Price,
         //      9=Platinum, 10=Titanium, 11=Aluminum, 12=Rhodium, 13=Rhuthenium, 14=Iridium, )
 
-        public double[,] MakePlanetList(int numPlanets)
+        public double[,] BigBang(int numPlanets)
         {
             Random rnd = new Random();
             Random rndDouble = new Random();
@@ -57,6 +57,18 @@ namespace Space_Game
             }
             return planetList;
         }
+
+
+
+
+
+
+
+
+
+
+
+
 
         public string GetPlanetName(int planetNum, double[,] planetList)
         {
@@ -180,43 +192,121 @@ namespace Space_Game
                     seg2 = "woe";
                     break;
                 case 13:
-                    seg1 = "bon";
+                    seg2 = "bon";
                     break;
                 case 14:
-                    seg1 = "pre";
+                    seg2 = "pre";
                     break;
                 case 15:
-                    seg1 = "oni";
+                    seg2 = "oni";
                     break;
                 case 16:
-                    seg1 = "zio";
+                    seg2 = "zio";
                     break;
                 case 17:
-                    seg1 = "vem";
+                    seg2 = "vem";
                     break;
                 case 18:
-                    seg1 = "rt";
+                    seg2 = "rt";
                     break;
                 case 19:
-                    seg1 = "ta";
+                    seg2 = "ta";
                     break;
                 case 20:
-                    seg1 = "pi";
+                    seg2 = "pi";
                     break;
                 case 21:
-                    seg1 = "urn";
+                    seg2 = "urn";
                     break;
                 case 22:
-                    seg1 = "are";
+                    seg2 = "are";
                     break;
                 case 23:
-                    seg1 = "san";
+                    seg2 = "san";
                     break;
                 case 24:
-                    seg1 = "plu";
+                    seg2 = "plu";
                     break;
                 case 25:
-                    seg1 = "tap";
+                    seg2 = "tap";
+                    break;
+            }
+            switch (planetList[planetNum, 4])
+            {
+                case 1:
+                    seg3 = "sar";
+                    break;
+                case 2:
+                    seg3 = "key";
+                    break;
+                case 3:
+                    seg3 = "gre";
+                    break;
+                case 4:
+                    seg3 = "neh";
+                    break;
+                case 5:
+                    seg3 = "rak";
+                    break;
+                case 6:
+                    seg3 = "ren";
+                    break;
+                case 7:
+                    seg3 = "try";
+                    break;
+                case 8:
+                    seg3 = "del";
+                    break;
+                case 9:
+                    seg3 = "lev";
+                    break;
+                case 10:
+                    seg3 = "ry";
+                    break;
+                case 11:
+                    seg3 = "diee";
+                    break;
+                case 12:
+                    seg3 = "que";
+                    break;
+                case 13:
+                    seg3 = "xie";
+                    break;
+                case 14:
+                    seg3 = "de";
+                    break;
+                case 15:
+                    seg3 = "c";
+                    break;
+                case 16:
+                    seg3 = "poc";
+                    break;
+                case 17:
+                    seg3 = "za";
+                    break;
+                case 18:
+                    seg3 = "h";
+                    break;
+                case 19:
+                    seg3 = "n";
+                    break;
+                case 20:
+                    seg3 = "ter";
+                    break;
+                case 21:
+                    seg3 = "moo";
+                    break;
+                case 22:
+                    seg3 = "num";
+                    break;
+                case 23:
+                    seg3 = "dsm";
+                    break;
+                case 24:
+                    seg3 = "to";
+                    break;
+                case 25:
+                    seg3 = "lan";
                     break;
             }
             return seg1 + seg2 + seg3;
