80eefc006f4acf6e80550920f3e379f69aac1245
Michael Hopkins <LuckyMagnet77@gmail.com>
2018-08-31 11:47:20 -0400

    Did some work on buy

--- Space Game/Trading.cs
+++ Space Game/Trading.cs
@@ -46,17 +46,17 @@ namespace Space_Game
             return false;
         }
 
-        public void BuyThings(ref Ship myShip, ref Player_Stats player)
+        public void BuyThings(ref Ship myShip, ref Player_Stats player, Travel myUniverse)
         {
             int cargoWhere;
             int itemAmount;
-            string currentItemBuy;
+            int currentItemBuy;
             bool isGood = false;
             bool buy = false;
             do
             {
                 Console.WriteLine("Nice ship, what slot are we using for new cargo?");
-                Console.WriteLine("Enter the slot number or 0 to show your inventory.");
+                Console.WriteLine("Enter the slot number or 0 to show your inventory and planet prices.");
                 cargoWhere = Utility.GetInt(myShip.CargoSlots()); ///CargoWhere: where we are placing the new cargo
                 if (cargoWhere == 0) // show inventory
                 {
@@ -68,32 +68,59 @@ namespace Space_Game
                     {
                         Console.WriteLine($"You want to buy more {Utility.cargoName(myShip.inventory[cargoWhere, 0])}.");
                         Console.WriteLine($"How much {Utility.cargoName(myShip.inventory[cargoWhere, 0])} do you want to buy?");
-
-                    }
-                    else
-                    {
-                        Console.WriteLine("What do you want to buy?");
-                        currentItemBuy = Utility.cargoName(myShip.inventory[cargoWhere, 0]);
                         Console.WriteLine("How much to you want to buy?");
                         itemAmount = Utility.GetInt(myShip.SlotSize());
-
-                        if ((itemAmount + myShip.inventory[cargoWhere, 1]) > myShip.SlotSize())
+                        if (itemAmount == 0)
+                        {
+                            Console.WriteLine("Decided not to buy huh.");
+                            isGood = true;
+                        }
+                        else if ((itemAmount + myShip.inventory[cargoWhere, 1]) > myShip.SlotSize())
                         {
                             Console.WriteLine("There isn't enough space");
                         }
                         else
                         {
-                            cost = itemAmount * prices[myShip.inventory[cargoWhere, 0]];
-                            Utility.BuySellYN(cost, ref buy, 1, ref player);
-                            if (buy)
+                            cost = prices[myShip.inventory[cargoWhere, 0]] * itemAmount;
+                            Console.WriteLine($"That will cost {cost}.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("What do you want to buy?");
+                        currentItemBuy = Utility.GetInt(9);
+                        if (currentItemBuy == 0)
+                        {
+                            Console.WriteLine("It's ok if you don't want to buy anything.");
+                            isGood = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("How much to you want to buy?");
+                            itemAmount = Utility.GetInt(myShip.SlotSize());
+                            if (itemAmount == 0)
                             {
-                                myShip.inventory[cargoWhere, 1] += itemAmount;
-                                cost = 0;
+                                Console.WriteLine("Decided not to buy huh.");
+                                isGood = true;
+                            }
+                            else if ((itemAmount + myShip.inventory[cargoWhere, 1]) > myShip.SlotSize())
+                            {
+                                Console.WriteLine("There isn't enough space");
                             }
                             else
                             {
-                                Console.WriteLine("Mayber another time.");
-                                cost = 0;
+                                cost = itemAmount * prices[myShip.inventory[cargoWhere, 0]];
+                                Utility.BuySellYN(cost, ref buy, 1, ref player);
+                                if (buy)
+                                {
+                                    myShip.inventory[cargoWhere, 1] += itemAmount;
+                                    cost = 0;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Mayber another time.");
+                                    cost = 0;
+                                }
                             }
                         }
                     }
